93daf6bf4b0387fe1d0ef5eda448f1dcd5204b85
RedGnom <147233214+RedGnom@users.noreply.github.com>
2024-02-12 12:19:23 +0500

    6 лаба 2 семестр(Фибоначчи)

---
+++ Числа фибоначии.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int f(int n) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return f(n - 2) + f(n - 1);
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    cout << "Число Фибоначчи " << f(12);
+    return 0;
+}
